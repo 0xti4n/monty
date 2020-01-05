796928db1c1aeb81c3f52ed2f16b535ccde20c4f
cristian gomez <1154@holbertonschool.com>
2020-01-05 19:54:47 +0000

    Create function free_error and modificate lines

--- _verification.c
+++ _verification.c
@@ -1,49 +1,75 @@
 #include "monty.h"
-char *filter_num(char * instruction)
+
+/**
+* free_error - Functions that free the information.
+* @ins: the instructions.
+* @head: The stack.
+* @fd: The file.
+* @line: The buf.
+* Return: Exit failure.
+*/
+void free_error(char **ins, stack_t **head, FILE *fd, char *line)
+{
+	free(ins);
+	free_stack(*head);
+	fclose(fd);
+	free(line);
+	exit(EXIT_FAILURE);
+}
+
+/**
+* filter_num - Functions that filter the information.
+* @instruction: instructions
+* Return: Null if not match.
+*/
+char *filter_num(char *instruction)
 {
 	int i = 0;
 
 	while (instruction[i])
 	{
-		if (!(instruction[i] >= 48 && instruction[i] <= 57) && instruction[i] != '-')
+		if (!(instruction[i] >= 48 && instruction[i] <= 57) && instruction[0] != '-')
 			return (NULL);
 		i++;
 	}
 	return (instruction);
 }
 /**
-* verification: verificate the information and execute.
+* verification -verificate the information and execute.
 * @head: The new node.
 * @line: The getline.
+* @n_line: NUmber of line.
+* @fd: The file.
 * Return: Nothing.
 */
 void verification(stack_t **head, char *line, int n_line, FILE *fd)
 {
 	int n_op = 0, not_match = 0;
-	char **instructions = NULL;
+	char **ins = NULL;
 
 	instruction_t comands[] = {
 		{"push", add_stack},
 		{"pall", print_all},
+		/**{"pint", pint_fun},
+		{"pop", pop_fun},
+		{"swap", swap_fun},
+		{"add", add_fun},
+		{"nop", nop_fun},*/
 		{NULL, NULL}
 	};
-	instructions = tokenize(line);
-	while (comands[n_op].opcode && instructions)
+	ins = tokenize(line);
+	while (comands[n_op].opcode && ins)
 	{
-		if (strcmp(comands[n_op].opcode, instructions[0]) == 0)
+		if (strcmp(comands[n_op].opcode, ins[0]) == 0)
 		{
 			if (strcmp(comands[n_op].opcode, "pall") != 0)
-			{	
-				if (instructions[1] == NULL || filter_num(instructions[1]) == NULL)
+			{
+				if (ins[1] == NULL || filter_num(ins[1]) == NULL)
 				{
 					dprintf(STDERR_FILENO, "L%d: usage: push integer\n", n_line);
-					free(instructions);
-					free_stack(*head);
-					fclose(fd);
-					free(line);
-					exit (EXIT_FAILURE);
+					free_error(ins, head, fd, line);
 				}
-				n = atoi(instructions[1]);
+				n = atoi(ins[1]);
 			}
 			comands[n_op].f(head, n_line);
 		}
@@ -51,14 +77,10 @@ void verification(stack_t **head, char *line, int n_line, FILE *fd)
 			not_match++;
 		n_op++;
 	}
-	if (not_match == n_op && instructions != NULL)
+	if (not_match == n_op && ins != NULL)
 	{
-		dprintf(STDERR_FILENO, "L%d: unknown instruction %s\n", n_line, instructions[0]);
-		free(instructions);
-		free_stack(*head);
-		fclose(fd);
-		free(line);
-		exit (EXIT_FAILURE);
+		dprintf(STDERR_FILENO, "L%d: unknown instruction %s\n", n_line, ins[0]);
+		free_error(ins, head, fd, line);
 	}
-	free(instructions);
+	free(ins);
 }
