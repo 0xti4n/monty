3d6782033cbcf6d0a1cda151619b8b679dbff30b
Cristian Gomez <1154@holbertonschool.com>
2020-01-06 03:53:16 +0000

    Fixed mul fun

--- _mul_fun.c
+++ _mul_fun.c
@@ -9,16 +9,17 @@
 void mul_fun(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp = *stack;
-	int sum = 0;
+	int mul = 0, mul2 = 0;
 
 	if (stack ==  NULL || *stack == NULL || (*stack)->next == NULL)
 	{
 		dprintf(STDERR_FILENO, "L%d: can't mul, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	sum = (*stack)->n;
+	mul = (*stack)->n;
+	mul2 = (*stack)->next->n;
 	*stack = (*stack)->next;
-	(*stack)->n *= sum;
+	(*stack)->n = mul * mul2;
 	if (*stack != NULL)
 		(*stack)->prev = NULL;
 	free(tmp);
