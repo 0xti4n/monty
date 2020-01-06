ff5a29abb4434aafe1089785ac5a07e1af5e7e00
joldiazch <jluis.diaz@udea.edu.co>
2020-01-06 00:51:11 +0000

    fixed the conditional no push case

--- _verification.c
+++ _verification.c
@@ -1,5 +1,4 @@
 #include "monty.h"
-
 /**
 * free_error - Functions that free the information.
 * @ins: the instructions.
@@ -62,7 +61,7 @@ void verification(stack_t **head, char *line, int n_line, FILE *fd)
 	{
 		if (strcmp(comands[n_op].opcode, ins[0]) == 0)
 		{
-			if (strcmp(comands[n_op].opcode, "pall") != 0 && strcmp(comands[n_op].opcode, "pint") != 0 && strcmp(comands[n_op].opcode, "pop") != 0 && strcmp(comands[n_op].opcode, "swap") != 0 && strcmp(comands[n_op].opcode, "add") != 0 && strcmp(comands[n_op].opcode, "nop") != 0)
+			if (strcmp(comands[n_op].opcode, "push") == 0)
 			{
 				if (ins[1] == NULL || filter_num(ins[1]) == NULL)
 				{
